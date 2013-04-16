9b64dc094b3792c290b869c18767e7dd1fbce047
Richard Scott Smith <scott@thescene.com>
2013-04-15 20:54:41 -0700

    Initial import of Lucene indexing project example.

---
+++ src/main/java/net/raescott/App.java
@@ -0,0 +1,10 @@
+package net.raescott;
+
+/**
+ * @author Richard Scott Smith <scott.smith@isostech.com>
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
